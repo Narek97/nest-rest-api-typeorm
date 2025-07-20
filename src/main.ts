2853e108216ca8bda0c257190476bd089ceb475d
Narek97 <narek.babayan.97.27@gmail.com>
2025-07-20 15:45:43 +0400

    swagger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionFilter } from './common/filters/all-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+
   app.useGlobalFilters(new AllExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Movie API')
+    .setDescription('Movie API')
+    .setVersion('1.0')
+    .setContact('Your Name', 'https://yourwebsite.com', 'user@gmail.com')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -13,21 +13,63 @@ import { MovieEntity } from './entities/movie.entity';
 import { MovieDto } from './dto/movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
 import { StringToLowercasePipe } from '../common/pipes/string-to-lowercase.pipe';
+import {
+  ApiBody,
+  ApiHeader,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Movie')
 @Controller('movie')
 export class MovieController {
   constructor(private readonly movieService: MovieService) {}
 
+  @ApiOperation({
+    summary: 'Get all movies',
+    description: 'Retrieve a list of all movies in the database.',
+  })
   @Get()
   async findAll(): Promise<MovieEntity[]> {
     return this.movieService.findAll();
   }
 
+  @ApiOperation({
+    summary: 'Create a new movie',
+    description: 'Add a new movie to the database.',
+  })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        title: { type: 'string', example: 'Inception' },
+        director: { type: 'string', example: 'Christopher Nolan' },
+        releaseDate: { type: 'number', format: 'date', example: 2025 },
+        genre: { type: 'string', example: 'Science Fiction' },
+      },
+    },
+  })
   @Post('')
   async create(@Body() dto: MovieDto): Promise<MovieEntity> {
     return this.movieService.create(dto);
   }
 
+  @ApiOperation({
+    summary: 'Get a movie by ID',
+    description: 'Retrieve a specific movie by its unique identifier.',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'The unique identifier of the movie',
+    type: Number,
+  })
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Bearer token for authentication',
+    required: true,
+    example: 'Bearer your_token_here',
+  })
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<MovieEntity> {
     return this.movieService.findOne(Number(id));
